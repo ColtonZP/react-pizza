a1757e20f0240e49b03e0e64509038352e1edd70
ColtonZP <39172201+ColtonZP@users.noreply.github.com>
2020-02-12 20:42:27 -0800

    Updates

---
+++ precache-manifest.3dc9f238ab9ae2f6f1244cb15d1732d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ab45618bab4265502b58ad7c260c4f",
+    "url": "/react-pizza/index.html"
+  },
+  {
+    "revision": "35149c34f59cab77919c",
+    "url": "/react-pizza/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ceac9b28182ade4be739",
+    "url": "/react-pizza/static/js/2.ee5d3608.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-pizza/static/js/2.ee5d3608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35149c34f59cab77919c",
+    "url": "/react-pizza/static/js/main.e37e97e7.chunk.js"
+  },
+  {
+    "revision": "ecb728bf6226f513ca03",
+    "url": "/react-pizza/static/js/runtime-main.b417e4d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pizza/static/media/logo.5d5d9eef.svg"
+  }
+]);
